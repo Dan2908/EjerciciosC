96f168b533d03f8026e92819042a9925f93c7ddd
Dan2908 <danilobrandolin29@gmail.com>
2021-06-14 17:44:56 -0300

    actalizar

--- Unidad IV/repaso/TP Repaso/ejercicio_1.c
+++ Unidad IV/repaso/TP Repaso/ejercicio_1.c
@@ -3,86 +3,76 @@
 #include <ctype.h>
 #define PAUSA fflush(stdin); getchar()
 #define FIN(x) printf(x); PAUSA; return 0
+#define MAX 25
 
-#define SE_PARRAFO '*'
-#define SE_ORACION '.'
-#define SE_PALABRA '-'
-#define F_PARRAFO "-.*"
-
-const static unsigned MAX = 250;
-static unsigned L = 0;
+#define SE_ORA '.'
+#define SE_PAL '-'
+#define F_PAR "-.*"
 
 typedef enum leer{
-    LEER_OK,
-    LEER_ERROR
+    LEER_ERROR,
+    LEER_OK
 } Lector;
 
-Lector error(){
-    fprintf(stderr, " --> Error: El parrafo supera la longitud maxima %u, no se agrega.", MAX - L);
-    return LEER_ERROR;
-}
-
-Lector evaluar(){
-    if(feof(stdin))
-        return error();
+Lector leer(char * output, unsigned tam){
+    if(tam == 1)
+        *output = tolower(getchar());
+    else{
+        if(fgets(output, tam, stdin) == NULL || ferror(stdin)){
+            fflush(stdin);
+            return LEER_ERROR;
+        }
+        if(output[tam - 1] != '\0'){
+            fflush(stdin);
+            fprintf(stderr, " x-> Error al leer, la linea ingresada supera el limite maximo (%u)\n", tam);
+            return LEER_ERROR;
+        }
+    }
+    fflush(stdin);
     return LEER_OK;
 }
 
-Lector leer(char *cadena){
-    char aux[MAX - L];
-    fgets(aux, MAX - L, stdin);
-    fflush(stdin);
-    if(evaluar() == LEER_ERROR)
-        return error();
-    else
-        strcat(cadena, aux);
-    return evaluar();
-}
-
-void marcar_parrafo(char *cadena){
-    char *p = strchr(cadena, '\n');
-    *p = SE_PARRAFO;
+void leer_parrafo(char *cadena, unsigned *L){
+    static char nuevo[MAX];
+    unsigned ultimo;
+    //Leer
+    if( ! leer(nuevo, *L)){
+        fprintf(stderr, " x-> Error al leer parrafo\n");
+        return;
+    }
+    if(nuevo[*L] != '\0'){
+        fprintf(stderr, " x-> Error: el parrafo supera el limite disponible de caracteres (%u)\n", L);
+        return;
+    }
+    //borrar el caracter nueva linea '\n'
+    *strchr(nuevo, '\n') = '\0';
+    //Si termina en '.' (SE_ORA), consideramos oracion y lo borramos para agregarlo luego
+    if(nuevo[strlen(nuevo) - 1] == SE_ORA)
+        nuevo[strlen(nuevo) - 1] = '\0';
+    //se agrega fin de palabra, de oracion y de parrafo (F_PAR)
+    strcat(nuevo, F_PAR);
+    L = L - strlen(nuevo);
+    strcat(cadena, nuevo);
     return;
 }
 
-void marcar_oraciones(char *cadena){
-    char *p = strchr(cadena, SE_PARRAFO);
-    while(p != NULL){
-        printf("\n--- %s", p);
-        if( *(p-1) != SE_ORACION){
-            char aux[MAX];
-            strcpy(aux, p);
-            strcpy(p, ".");
-            printf("\n--- p: %s", p);
-            printf("\n--- aux: %s", aux);
-            strcat(cadena, aux);
-            printf("\n--- cad: %s", cadena);
-        }
-        p = strchr(p+2, SE_PARRAFO);
-    }
-}
-
-
-const char leer_caracter(){
-    char result = getchar();
-    fflush(stdin);
-    return result;
+void marca_palabras(char * cadena){
+    *strchr(cadena, ' ') = SE_PAL;
 }
 
 int main(){
-    char cadena[MAX];
-    cadena[0] = '\0';
-    char opc = 's';
+    char cadena[MAX], 
+         opcion;
+    unsigned L = MAX - 3;
 
-    while(opc == 's' || opc != 'n'){
-        printf("Agregar un parrafo [s/n]?");
-        opc = leer_caracter();
-        if(opc == 's'){
-            leer(cadena);
-            marcar_parrafo(cadena);
+    printf("\n    >>> Palabras, oraciones y parrafos <<<\n");
+    do{
+        printf("Agregar parrafo (s/n)? ");
+        leer(&opcion, 1);
+        if(opcion == 's'){
+            leer_parrafo(cadena, &L);
         }
-    }
-    marcar_oraciones(cadena);
+    }while(opcion != 'n');
 
     printf("%s", cadena);
 
